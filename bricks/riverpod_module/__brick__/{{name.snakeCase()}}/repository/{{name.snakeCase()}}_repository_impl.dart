9d08936bab0292ea8962f6ebaf69bfa3bddb1cdd
Preet Singh <preetsingh3001@gmail.com>
2023-05-06 23:42:52 +0530

    fix: g file path issue

--- bricks/riverpod_module/__brick__/{{name.snakeCase()}}/repository/{{name.snakeCase()}}_repository_impl.dart
+++ bricks/riverpod_module/__brick__/{{name.snakeCase()}}/repository/{{name.snakeCase()}}_repository_impl.dart
@@ -2,7 +2,7 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 import '{{name.snakeCase()}}_repository.dart';
 
-part '{{name.snakeCase()}}.g.dart';
+part '{{name.snakeCase()}}_repository_impl.g.dart';
 
 class {{name.pascalCase()}}RepositoryImpl extends {{name.pascalCase()}}Repository {
   // TODO add your methods here
--- bricks/riverpod_project/__brick__/lib/app/features/main/repository/main_repository_impl.dart
+++ bricks/riverpod_project/__brick__/lib/app/features/main/repository/main_repository_impl.dart
@@ -1,10 +1,12 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
 import 'main_repository.dart';
+
+part 'main_repository_impl.g.dart';
 
 class MainRepositoryImpl extends MainRepository {
   // TODO add your methods here
 }
 
-final mainRepositoryProvider = Provider<MainRepository>((ref) {
-  return MainRepositoryImpl();
-});
+@riverpod
+MainRepository mainRepository(MainRepositoryRef ref) => MainRepositoryImpl();
